5492a6dd83a47585b147e8fdb10fa4a2725ccc2d
Felix Schnizlein <felix@schnizle.in>
2016-09-22 10:21:35 +0200

    add wordpress; apache -> webserver

--- balancer/balancer.tf
+++ balancer/balancer.tf
@@ -1,6 +1,6 @@
 
 output "ip" {
-  value = "${openstack_compute_instance_v2.mysql.access_ip_v4}"
+  value = "${openstack_compute_instance_v2.balancer.access_ip_v4}"
 }
 
 variable "public_key" {
@@ -20,25 +20,26 @@ variable "flavor_name" {
   default = "m1.smaller"
 }
 
-resource "openstack_compute_keypair_v2" "mysql_key" {
-  name="interop-mysql-key"
+resource "openstack_compute_keypair_v2" "balancer_key" {
+  name="interop-balancer-key"
   public_key="${var.public_key}"
 }
 
-resource "openstack_compute_floatingip_v2" "mysql_floating" {
+resource "openstack_compute_floatingip_v2" "balancer_floating" {
   pool = "floating"
 }
 
-resource "openstack_compute_instance_v2" "mysql" {
+resource "openstack_compute_instance_v2" "balancer" {
     region = ""
-    name = "interop-mysql-1"
+    name = "interop-balancer-1"
     image_name = "${var.image_name}"
     flavor_name = "${var.flavor_name}"
-    key_pair = "interop-mysql-key"
+    key_pair = "interop-balancer-key"
     security_groups = ["default"]
+
     network {
         access_network = true
         name = "fixed"
-        floating_ip = "${openstack_compute_floatingip_v2.mysql_floating.address}"
+        floating_ip = "${openstack_compute_floatingip_v2.balancer_floating.address}"
     }
 }
--- webserver/webserver.tf
+++ webserver/webserver.tf
@@ -1,10 +1,10 @@
 
 output "ip" {
-  value = "${join(" ", openstack_compute_instance_v2.apache.*.access_ip_v4)}"
+  value = "${join(" ", openstack_compute_instance_v2.webserver.*.access_ip_v4)}"
 }
 
-variable "apache_count" {
-  description = "number of apache hosts"
+variable "webserver_count" {
+  description = "number of webserver hosts"
   type = "string"
   default = "1"
 }
@@ -26,48 +26,45 @@ variable "flavor_name" {
   default = "m1.smaller"
 }
 
-resource "openstack_compute_keypair_v2" "apache_key" {
-  name="interop-apache-key"
+resource "openstack_compute_keypair_v2" "webserver_key" {
+  name="interop-webserver-key"
   public_key="${var.public_key}"
 }
 
-resource "openstack_compute_floatingip_v2" "apache_floating" {
-  count= "${var.apache_count}"
+resource "openstack_compute_floatingip_v2" "webserver_floating" {
+  count= "${var.webserver_count}"
   pool = "floating"
 }
 
-resource "openstack_compute_secgroup_v2" "lamp_security_group" {
-  name = "interop-security-group"
-  description = "Interop challange security group"
-  rule {
-    from_port = 22
-    to_port = 22
-    ip_protocol = "tcp"
-    cidr = "0.0.0.0/0"
-  }
-  rule {
-    from_port = 80
-    to_port = 80
-    ip_protocol = "tcp"
-    cidr = "0.0.0.0/0"
-  }
-}
+/* resource "openstack_compute_secgroup_v2" "lamp_security_group" { */
+/*   name = "interop" */
+/*   description = "Interop challange security group" */
+/*   rule { */
+/*     from_port = 22 */
+/*     to_port = 22 */
+/*     ip_protocol = "tcp" */
+/*     cidr = "0.0.0.0/0" */
+/*   } */
+/*   rule { */
+/*     from_port = 80 */
+/*     to_port = 80 */
+/*     ip_protocol = "tcp" */
+/*     cidr = "0.0.0.0/0" */
+/*   } */
+/* } */
 
-resource "openstack_compute_instance_v2" "apache" {
+resource "openstack_compute_instance_v2" "webserver" {
     region = ""
-    count = "${var.apache_count}"
-    name = "interop-apache-${count.index}"
+    count = "${var.webserver_count}"
+    name = "interop-webserver-${count.index}"
     image_name = "${var.image_name}"
     flavor_name = "${var.flavor_name}"
-    key_pair = "interop-apache-key"
-    security_groups = ["interop-security-group"]
-    metadata {
-        demo = "metadata"
-    }
+    key_pair = "interop-webserver-key"
+    security_groups = ["default"]
 
     network {
         access_network = true
         name = "fixed"
-        floating_ip = "${element(openstack_compute_floatingip_v2.apache_floating.*.address, count.index)}"
+        floating_ip = "${element(openstack_compute_floatingip_v2.webserver_floating.*.address, count.index)}"
     }
 }
